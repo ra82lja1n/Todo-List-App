f78d75dd1b8619cf8d13fada673a04d832a62efe
Rahul Jain <rj7983791@gmail.com>
2026-03-01 20:47:02 +0530

    Adding files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from "./Component/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{textAlign : 'center'}}>TODO LIST APP</h1>
+      <Todo/>
+    </div>
+  );
+};
+
+export default App
